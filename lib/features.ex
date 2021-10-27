c5153775a097f3a348cac0163f02fcb9645e39be
danielefongo <danielefongo@gmail.com>
2021-10-27 14:25:10 +0200

    add simple special annotation for functions

--- lib/features.ex
+++ lib/features.ex
@@ -1,8 +1,12 @@
 defmodule Features do
   @moduledoc false
 
+  import Kernel, except: [def: 2]
+
   defmacro __using__(_) do
     quote do
+      import Kernel, except: [def: 2]
+
       @enabled_features Application.compile_env!(:features, :features)
 
       require Features
@@ -23,6 +27,34 @@ defmodule Features do
       if unquote(feature) in @enabled_features do
         unquote(rest)
       end
+    end
+  end
+
+  defmacro def(call, expr) do
+    quote do
+      feature = Module.get_attribute(__MODULE__, :feature)
+      feature_off = Module.get_attribute(__MODULE__, :feature_off)
+
+      cond do
+        feature != nil && feature_off != nil ->
+          raise "Cannot use feature and feature_off"
+
+        feature != nil ->
+          if feature in @enabled_features do
+            Kernel.def(unquote(call), unquote(expr))
+          end
+
+        feature_off != nil ->
+          if feature_off not in @enabled_features do
+            Kernel.def(unquote(call), unquote(expr))
+          end
+
+        true ->
+          Kernel.def(unquote(call), unquote(expr))
+      end
+
+      Module.delete_attribute(__MODULE__, :feature)
+      Module.delete_attribute(__MODULE__, :feature_off)
     end
   end
 end
--- test/features_test.exs
+++ test/features_test.exs
@@ -2,24 +2,85 @@ defmodule FeaturesTest do
   use ExUnit.Case
   use Features
 
-  test "check enabled feature" do
-    assert true ==
-             (feature :feature_x do
-                true
-              end)
+  describe "simple macro" do
+    test "check enabled feature" do
+      assert true ==
+               (feature :feature_x do
+                  true
+                end)
+    end
+
+    test "check not enabled feature" do
+      assert true ==
+               (no_feature :feature_y do
+                  true
+                end)
+    end
+
+    test "not enabled feature generate nil code" do
+      assert nil ==
+               (feature :feature_y do
+                  :should_not_return_this
+                end)
+    end
   end
 
-  test "check not enabled feature" do
-    assert true ==
-             (no_feature :feature_y do
-                true
-              end)
-  end
+  describe "function annotation" do
+    test "feature on" do
+      defmodule FeatureOn do
+        use Features
 
-  test "not enabled feature generate nil code" do
-    assert nil ==
-             (feature :feature_y do
-                :should_not_return_this
-              end)
+        @feature :feature_x
+        def hello, do: :hello
+      end
+
+      assert FeatureOn.hello() == :hello
+    end
+
+    test "feature off" do
+      defmodule FeatureOff do
+        use Features
+
+        @feature_off :feature_y
+        def hello, do: :hello
+      end
+
+      assert FeatureOff.hello() == :hello
+    end
+
+    test "feature on for undefined feature" do
+      defmodule FeatureOnForUndefniedFeature do
+        use Features
+
+        @feature :feature_y
+        def hello, do: :hello
+      end
+
+      assert_raise UndefinedFunctionError, fn -> FeatureOnForUndefniedFeature.hello() end
+    end
+
+    test "feature off for defined feature" do
+      defmodule FeatureOffForDefinedFeature do
+        use Features
+
+        @feature_off :feature_x
+        def hello, do: :hello
+      end
+
+      assert_raise UndefinedFunctionError, fn -> FeatureOffForDefinedFeature.hello() end
+    end
+
+    test "feature off not propagated" do
+      defmodule FeatureOffNotPropagated do
+        use Features
+
+        @feature_off :feature_x
+        def hello, do: :hello
+
+        def present?, do: true
+      end
+
+      assert FeatureOffNotPropagated.present?() == true
+    end
   end
 end
